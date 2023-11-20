7f41bbf0f322470007e6e9df994dd5f7ecfb28ff
SahaidakYurii <sahaidak.pn@ucu.edu.ua>
2023-11-20 18:11:26 +0200

    working web-server from tutorial

--- main/main.c
+++ main/main.c
@@ -1,179 +1,64 @@
-#include <esp_wifi.h>
-#include <esp_event_loop.h>
-#include <esp_log.h>
-#include <esp_timer.h>
 #include <esp_system.h>
 #include <nvs_flash.h>
-#include <sys/param.h>
+#include <esp_log.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
 
-#include <esp_http_server.h>
 #include "esp_camera.h"
-#define BOARD_WROVER_KIT 1
+#include "esp_http_server.h"
+#include "esp_timer.h"
 
-// WROVER-KIT PIN Map
-#ifdef BOARD_WROVER_KIT
+#include "camera_pins.h"
+#include "connect_wifi.h"
 
-#define CAM_PIN_PWDN -1  //power down is not used
-#define CAM_PIN_RESET -1 //software reset will be performed
-#define CAM_PIN_XCLK 32
-#define CAM_PIN_SIOD 21
-#define CAM_PIN_SIOC 22
+static const char *TAG = "esp32-cam Webserver";
 
-#define CAM_PIN_D7 4
-#define CAM_PIN_D6 12
-#define CAM_PIN_D5 13
-#define CAM_PIN_D4 14
-#define CAM_PIN_D3 15
-#define CAM_PIN_D2 16
-#define CAM_PIN_D1 17
-#define CAM_PIN_D0 27
-
-#define CAM_PIN_VSYNC 25
-#define CAM_PIN_HREF 35
-#define CAM_PIN_PCLK 33
-
-#endif
-
-// ESP32Cam (AiThinker) PIN Map
-#ifdef BOARD_ESP32CAM_AITHINKER
-
-#define CAM_PIN_PWDN 32
-#define CAM_PIN_RESET -1 //software reset will be performed
-#define CAM_PIN_XCLK 0
-#define CAM_PIN_SIOD 26
-#define CAM_PIN_SIOC 27
-
-#define CAM_PIN_D7 35
-#define CAM_PIN_D6 34
-#define CAM_PIN_D5 39
-#define CAM_PIN_D4 36
-#define CAM_PIN_D3 21
-#define CAM_PIN_D2 19
-#define CAM_PIN_D1 18
-#define CAM_PIN_D0 5
-#define CAM_PIN_VSYNC 25
-#define CAM_PIN_HREF 23
-#define CAM_PIN_PCLK 22
-
-#endif
-
-static const char *TAG = "example:http_jpg";
-/****************** Camera configuration *******************/
-static camera_config_t camera_config = {
-        // DRAM or PSRAM
-#ifdef BOARD_WROVER_KIT
-    .fb_location = CAMERA_FB_IN_DRAM,
-#else
-    .fb_location = CAMERA_FB_IN_PSRAM,
-#endif
-
-    .pin_pwdn = CAM_PIN_PWDN,
-    .pin_reset = CAM_PIN_RESET,
-    .pin_xclk = CAM_PIN_XCLK,
-    .pin_sccb_sda = CAM_PIN_SIOD,
-    .pin_sccb_scl = CAM_PIN_SIOC,
-
-    .pin_d7 = CAM_PIN_D7,
-    .pin_d6 = CAM_PIN_D6,
-    .pin_d5 = CAM_PIN_D5,
-    .pin_d4 = CAM_PIN_D4,
-    .pin_d3 = CAM_PIN_D3,
-    .pin_d2 = CAM_PIN_D2,
-    .pin_d1 = CAM_PIN_D1,
-    .pin_d0 = CAM_PIN_D0,
-    .pin_vsync = CAM_PIN_VSYNC,
-    .pin_href = CAM_PIN_HREF,
-    .pin_pclk = CAM_PIN_PCLK,
-
-    //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
-    .xclk_freq_hz = 10000000,
-    .ledc_timer = LEDC_TIMER_0,
-    .ledc_channel = LEDC_CHANNEL_0,
-
-    .pixel_format = PIXFORMAT_JPEG, //YUV422,GRAYSCALE,RGB565,JPEG
-    .frame_size = FRAMESIZE_QQVGA,    //QQVGA-UXGA, For ESP32, do not use sizes above QVGA when not JPEG. The performance of the ESP32-S series has improved a lot, but JPEG mode always gives better frame rates.
-
-    .jpeg_quality = 12, //0-63, for OV series camera sensors, lower number means higher quality
-    .fb_count = 1,       //When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode.
-    .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
-};
-
-static esp_err_t init_camera()
-{
-    //initialize the camera
-    esp_err_t err = esp_camera_init(&camera_config);
-    if (err != ESP_OK)
-    {
-        ESP_LOGE(TAG, "Camera Init Failed");
-        return err;
-    }
-    sensor_t * s = esp_camera_sensor_get();
-
-    //drop down frame size for higher initial frame rate
-    s->set_framesize(s, FRAMESIZE_QVGA);
-
-    return ESP_OK;
-}
-
-/*
-typedef struct
-{
-  httpd_req_t *req;
-  size_t len;
-} jpg_chunking_t;
-
-static size_t jpg_encode_stream(void *arg, size_t index, const void *data, size_t len)
-{
-  jpg_chunking_t *j = (jpg_chunking_t *)arg;
-  if (!index)
-  {
-    j->len = 0;
-  }
-  if (httpd_resp_send_chunk(j->req, (const char *)data, len) != ESP_OK)
-  {
-    return 0;
-  }
-  j->len += len;
-  return len;
-}
-
-static esp_err_t jpg_httpd_handler(httpd_req_t *req)
-{
-  camera_fb_t *fb = NULL;
-  esp_err_t res = ESP_OK;
-  size_t fb_len = 0;
-  int64_t fr_start = esp_timer_get_time();
-
-  fb = esp_camera_fb_get();
-  if (!fb)
-  {
-    ESP_LOGE(TAG, "Camera capture failed");
-    httpd_resp_send_500(req);
-    return ESP_FAIL;
-  }
-  res = httpd_resp_set_type(req, "image/jpeg");
-  if (res == ESP_OK)
-  {
-    res = httpd_resp_set_hdr(req, "Content-Disposition", "inline; filename=capture.jpg");
-  }
-
-  if (res == ESP_OK)
-  {
-    fb_len = fb->len;
-    res = httpd_resp_send(req, (const char *)fb->buf, fb->len);
-  }
-  esp_camera_fb_return(fb);
-  int64_t fr_end = esp_timer_get_time();
-  ESP_LOGI(TAG, "JPG: %uKB %ums", (uint32_t)(fb_len / 1024), (uint32_t)((fr_end - fr_start) / 1000));
-  return res;
-}
-*/
-
-/****************** HTTP jpg streaming *******************/
 #define PART_BOUNDARY "123456789000000000000987654321"
 static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
 static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
 static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
+#define CONFIG_XCLK_FREQ 20000000 
+
+static esp_err_t init_camera(void)
+{
+    camera_config_t camera_config = {
+        .pin_pwdn  = CAM_PIN_PWDN,
+        .pin_reset = CAM_PIN_RESET,
+        .pin_xclk = CAM_PIN_XCLK,
+        .pin_sccb_sda = CAM_PIN_SIOD,
+        .pin_sccb_scl = CAM_PIN_SIOC,
+
+        .pin_d7 = CAM_PIN_D7,
+        .pin_d6 = CAM_PIN_D6,
+        .pin_d5 = CAM_PIN_D5,
+        .pin_d4 = CAM_PIN_D4,
+        .pin_d3 = CAM_PIN_D3,
+        .pin_d2 = CAM_PIN_D2,
+        .pin_d1 = CAM_PIN_D1,
+        .pin_d0 = CAM_PIN_D0,
+        .pin_vsync = CAM_PIN_VSYNC,
+        .pin_href = CAM_PIN_HREF,
+        .pin_pclk = CAM_PIN_PCLK,
+
+        .xclk_freq_hz = CONFIG_XCLK_FREQ,
+        .ledc_timer = LEDC_TIMER_0,
+        .ledc_channel = LEDC_CHANNEL_0,
+
+        .pixel_format = PIXFORMAT_JPEG,
+        .frame_size = FRAMESIZE_VGA,
+
+        .jpeg_quality = 10,
+        .fb_count = 1,
+        .grab_mode = CAMERA_GRAB_WHEN_EMPTY};//CAMERA_GRAB_LATEST. Sets when buffers should be filled
+    esp_err_t err = esp_camera_init(&camera_config);
+    if (err != ESP_OK)
+    {
+        return err;
+    }
+    return ESP_OK;
+}
 
 esp_err_t jpg_stream_httpd_handler(httpd_req_t *req){
     camera_fb_t * fb = NULL;
@@ -199,7 +84,7 @@ esp_err_t jpg_stream_httpd_handler(httpd_req_t *req){
             break;
         }
         if(fb->format != PIXFORMAT_JPEG){
-            bool jpeg_converted = frame2bmp(fb, &_jpg_buf, &_jpg_buf_len);
+            bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
             if(!jpeg_converted){
                 ESP_LOGE(TAG, "JPEG compression failed");
                 esp_camera_fb_return(fb);
@@ -221,9 +106,9 @@ esp_err_t jpg_stream_httpd_handler(httpd_req_t *req){
         if(res == ESP_OK){
             res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
         }
-       if(fb->format != PIXFORMAT_JPEG){
-           free(_jpg_buf);
-       }
+        if(fb->format != PIXFORMAT_JPEG){
+            free(_jpg_buf);
+        }
         esp_camera_fb_return(fb);
         if(res != ESP_OK){
             break;
@@ -232,133 +117,58 @@ esp_err_t jpg_stream_httpd_handler(httpd_req_t *req){
         int64_t frame_time = fr_end - last_frame;
         last_frame = fr_end;
         frame_time /= 1000;
-        ESP_LOGI(TAG, "MJPG: %luKB %lums (%.1ffps)",
-                 (uint32_t)(_jpg_buf_len/1024),
-                 (uint32_t)frame_time, 1000.0 / (uint32_t)frame_time);
+        ESP_LOGI(TAG, "MJPG: %uKB %ums (%.1ffps)",
+            (uint16_t)(_jpg_buf_len/1024),
+            (uint16_t)frame_time, 1000.0 / (uint16_t)frame_time);
     }
 
     last_frame = 0;
     return res;
 }
 
-
-httpd_uri_t uri_handler_jpg = {
-        .uri = "/stream.bmp",
-        .method = HTTP_GET,
-        .handler = jpg_stream_httpd_handler};
-
-
-httpd_handle_t start_webserver(void)
+httpd_uri_t uri_get = {
+    .uri = "/",
+    .method = HTTP_GET,
+    .handler = jpg_stream_httpd_handler,
+    .user_ctx = NULL};
+httpd_handle_t setup_server(void)
 {
-    httpd_handle_t server = NULL;
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    httpd_handle_t stream_httpd  = NULL;
 
-    // Start the httpd server
-    ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
-    if (httpd_start(&server, &config) == ESP_OK)
+    if (httpd_start(&stream_httpd , &config) == ESP_OK)
     {
-        // Set URI handlers
-        ESP_LOGI(TAG, "Registering URI handlers");
-        httpd_register_uri_handler(server, &uri_handler_jpg);
-        httpd_register_uri_handler(server, &uri_handler_jpg1);
-        return server;
+        httpd_register_uri_handler(stream_httpd , &uri_get);
     }
 
-    ESP_LOGI(TAG, "Error starting server!");
-    return NULL;
-}
-
-void stop_webserver(httpd_handle_t server)
-{
-    // Stop the httpd server
-    httpd_stop(server);
-}
-/****************** Wifi event handler *******************/
-static void event_handler(void* ctx, esp_event_base_t *event, int32_t event_id, void* event_data)
-{
-    httpd_handle_t *server = (httpd_handle_t *)ctx;
-
-    switch (event_id)
-    {
-        case WIFI_EVENT_STA_START:
-            ESP_LOGI(TAG, "SYSTEM_EVENT_STA_START");
-            ESP_ERROR_CHECK(esp_wifi_connect());
-            break;
-        case IP_EVENT_ETH_GOT_IP:
-        case IP_EVENT_GOT_IP6:
-        case IP_EVENT_PPP_GOT_IP:
-        case IP_EVENT_STA_GOT_IP:
-        case ESP_NETIF_IP_EVENT_GOT_IP:
-            ESP_LOGI(TAG, "SYSTEM_EVENT_STA_GOT_IP");
-            ip_event_got_ip_t* param = (ip_event_got_ip_t*)event_data;
-            char str_ip[16];
-            ESP_LOGI(TAG, "Got IP: '%s'",
-                     esp_ip4addr_ntoa(&param->ip_info.ip, str_ip, IP4ADDR_STRLEN_MAX));
-//                        esp_ip4addr_ntoa(&event_data->));
-
-            /* Start the web server */
-            if (*server == NULL)
-            {
-                *server = start_webserver();
-            }
-            break;
-        case WIFI_EVENT_STA_DISCONNECTED:
-            ESP_LOGI(TAG, "SYSTEM_EVENT_STA_DISCONNECTED");
-            ESP_ERROR_CHECK(esp_wifi_connect());
-
-            /* Stop the web server */
-            if (*server)
-            {
-                stop_webserver(*server);
-                *server = NULL;
-            }
-            break;
-        default:
-            break;
-    }
-}
-
-static void initialise_wifi(void *arg)
-{
-//    tcpip_adapter_init();
-    esp_netif_init();
-
-    esp_event_handler_instance_t instance_any_id;
-    esp_event_handler_instance_t instance_got_ip;
-//    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, arg));
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(
-        WIFI_EVENT, ESP_EVENT_ANY_ID,
-        (esp_event_handler_t) &event_handler,
-        arg,
-        &instance_any_id
-    ));
-
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
-        IP_EVENT_STA_GOT_IP,
-        (esp_event_handler_t) &event_handler,
-        arg,
-        &instance_got_ip
-    ));
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
-    wifi_config_t wifi_config = {
-            .sta = {
-                    .ssid = "UCU_Guest",
-                    .password = "",
-            },
-    };
-    ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
+    return stream_httpd;
 }
 
 void app_main()
 {
-    static httpd_handle_t server = NULL;
-    ESP_ERROR_CHECK(nvs_flash_init());
-    init_camera();
-    initialise_wifi(&server);
+    esp_err_t err;
+
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+
+    connect_wifi();
+
+    if (wifi_connect_status)
+    {
+        err = init_camera();
+        if (err != ESP_OK)
+        {
+            printf("err: %s\n", esp_err_to_name(err));
+            return;
+        }
+        setup_server();
+        ESP_LOGI(TAG, "ESP32 CAM Web Server is up and running\n");
+    }
+    else
+        ESP_LOGI(TAG, "Failed to connected with Wi-Fi, check your network Credentials\n");
 }
