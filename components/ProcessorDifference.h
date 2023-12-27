be5440f71a5304f5bfae76dc54f785b0e53e811e
Victor Muryn <muryn.pn@ucu.edu.ua>
2023-12-27 23:40:29 +0200

    added faster version

--- components/ProcessorDifference.h
+++ components/ProcessorDifference.h
@@ -12,6 +12,17 @@
 #include <queue>
 #include <algorithm>
 
+struct Point {
+    size_t x, y;
+
+    Point(size_t x, size_t y) : x(x), y(y) {}
+};
+
+struct Rect {
+    Point first, second;
+    bool merged = false;
+    Rect (Point f, Point s) : first(f), second(s) {}
+};
 
 class ProcessorDifference: public virtual Processor {
 private:
@@ -24,7 +35,9 @@ private:
     int r = 5;
     bool* same;
 
-    void dfs(camera_fb_t* fb, size_t pos, bool draw);
+    Rect dfs(camera_fb_t* fb, size_t pos, bool draw);
+    std::vector<Rect> merge_rects(std::vector<Rect> rects);
+    void draw(const std::vector<Rect>& rects, camera_fb_t* fb);
 public:
     ProcessorDifference(Camera* camera);
     camera_fb_t* iterate() override;
